7b45d21816159acdff028e091e613acdaff56901
PROFYokada <yokadavitor@gmail.com>
2024-08-22 15:53:20 -0300

    Revert "Mudanças no arquivo em python aula 1"
    This reverts commit 42fa9e77c45700d05e793e2529d1f0df7faee91f.

--- semana18aula1.py
+++ semana18aula1.py
@@ -1,11 +1,11 @@
-def calcular_cliques_primeiro_link(p):
+def calcular_cliques_primeiro_link(t):
     # Verificando se t está no intervalo válido
-    if 1 <= p <= 1000:
-        return 4 * p
+    if 1 <= t <= 1000:
+        return 4 * t
     else:
         return "Entrada inválida"
 
 # Teste do programa
-p = int(input("Digite o número de pessoas que clicaram no terceiro link: "))
-resultado = calcular_cliques_primeiro_link(p)
+t = int(input("Digite o número de pessoas que clicaram no terceiro link: "))
+resultado = calcular_cliques_primeiro_link(t)
 print(resultado)
